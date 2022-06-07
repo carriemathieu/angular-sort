b001496f05f14a34dd8db1348854e924ed7e140f
Carrie Mathieu <carrieburks@gmail.com>
2022-06-07 17:11:07 -0400

    basic bubble sort

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-console.log('hi there')
+class Sorter {
 
-// run node build/index.js to run program
+    constructor(public collection: number[]) {
+    }
+
+    sort(): void {
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++ ){
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j+1]) {
+                    const leftElement = this.collection[j];
+                    this.collection[j] = this.collection[j+1];
+                    this.collection[j+1] = leftElement;
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0])
+sorter.sort();
+console.log(sorter.collection);
