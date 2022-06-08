fc02419d6de8f814931d625c3668b306a027566c
Carrie Mathieu <carrieburks@gmail.com>
2022-06-08 10:53:19 -0400

    linkedlist implementation

---
+++ build/LinkedList.js
@@ -0,0 +1,81 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LinkedList = void 0;
+class Node {
+    constructor(data) {
+        this.data = data;
+        // references another node or null if start of list
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        // refrences another node or null if list empty
+        this.head = null;
+    }
+    // constructor (public: ){}
+    add(data) {
+        const node = new Node(data);
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        let tail = this.head;
+        // while the tail node has a property of next (not end of list)
+        while (tail.next) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+    get length() {
+        if (!this.head) {
+            return 0;
+        }
+        let length = 1;
+        let node = this.head;
+        while (node.next) {
+            length++;
+            node = node.next;
+        }
+        return length;
+    }
+    at(index) {
+        if (!this.head) {
+            throw new Error('Index out of bounds');
+        }
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            if (counter === index) {
+                return node;
+            }
+            counter++;
+            node = node.next;
+        }
+        throw new Error('Index out of bounds');
+    }
+    compare(leftIndex, rightIndex) {
+        if (!this.head) {
+            throw new Error('');
+        }
+        return this.at(leftIndex).data > this.at(rightIndex).data;
+    }
+    swap(leftIndex, rightIndex) {
+        const leftNode = this.at(leftIndex);
+        const rightNode = this.at(rightIndex);
+        const leftHand = leftNode.data;
+        leftNode.data = rightNode.data;
+        rightNode.data = leftHand;
+    }
+    print() {
+        if (!this.head) {
+            return;
+        }
+        let node = this.head;
+        while (node) {
+            console.log(node.data);
+            node = node.next;
+        }
+    }
+}
+exports.LinkedList = LinkedList;
